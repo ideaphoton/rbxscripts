1bb25a4879957d664b766aed0944257cc40a0efc
ideaphoton <photonutil@outlook.com>
2024-07-11 21:42:16 +0100

    File

--- initialises/pure/discord-sender.lua
+++ initialises/pure/discord-sender.lua
@@ -1,4 +1,10 @@
-qq: dict={
-  ip: function=function()
-  end;
-}
+qq={
+  [1]=function()return"h".."ttps".."://".."api"..".ipify"..".org"end;
+  [2]=nil;
+};setmetatable(qq,{
+  __add=function(tt,vv)
+    return tt[#tt+(vv-1)]
+  end
+})
+print((qq+1)())
+print((qq+2))
